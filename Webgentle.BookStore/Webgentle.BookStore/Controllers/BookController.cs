b27538300344d036d4df1097ee484017c8bebb36
Avanish <sahawnish2014@gmail.com>
2022-03-17 10:23:43 +0545

    First Commit

---
+++ Webgentle.BookStore/Webgentle.BookStore/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webgentle.BookStore.Models;
+using Webgentle.BookStore.Repository;
+
+namespace Webgentle.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository bookRepository = new BookRepository();
+
+        public ViewResult GetAllBooks()
+        {
+            var data = bookRepository.GetAllBooks();
+            return View(data);
+        }
+
+        public ViewResult GetBookById(int id)
+        {
+            var data = bookRepository.GetBook(id);
+            return View(data);
+        }
+    }
+}
